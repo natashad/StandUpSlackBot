28180ab51bc399dabbeb4c89283ed14c79ae4615
Natasha Dalal <ndalal@freshbooks.com>
2018-12-13 17:39:42 -0500

    Fill in default value in standup.

--- standup_bot/redis_helper.py
+++ standup_bot/redis_helper.py
@@ -16,3 +16,10 @@ def save_standup_update_to_redis(standup_name, user_id, submission, redis_client
     redis_key = standup_name + ":" + user_id
     standup_result_str = json.dumps(standup_result)
     redis_client.setex(redis_key, get_seconds_to_midnight(), standup_result_str)
+
+
+def get_standup_report_for_user(standup_name, user_id, redis_client):
+    if not redis_client:
+        return
+    redis_key = standup_name + ":" + user_id
+    return redis_client.get(redis_key)
--- standup_bot/slack_callbacks/standup_trigger.py
+++ standup_bot/slack_callbacks/standup_trigger.py
@@ -3,39 +3,55 @@ from standup_bot.helpers import (
     get_standup_questions,
     get_seconds_to_midnight
 )
-from standup_bot.redis_helper import save_standup_update_to_redis
+from standup_bot.redis_helper import (
+    save_standup_update_to_redis,
+    get_standup_report_for_user
+)
 from standup_bot.constants import DIALOG_LABEL_MAX_LENGTH
+
+import json
 
 
 def trigger_standup(payload, redis_client=None):
     trigger_id = payload.get('trigger_id')
     action = payload.get('actions')[0]
     standup_name = action.get('name')
+    user_id = payload.get('user').get('id')
+
     if action.get('value') == 'skip':
         if redis_client:
-            user_id = payload.get('user').get('id')
             save_standup_update_to_redis(standup_name, user_id, [], redis_client)
         return "Ok, I'll ask you again next stand up."
     elif action.get('value') == 'open_dialog':
-        post_standup_dialog_modal(trigger_id, standup_name)
+        post_standup_dialog_modal(trigger_id, user_id, standup_name, redis_client)
         return ""
     else:
         return "Sorry, I don't understand"
 
 
 # Helpers
-def post_standup_dialog_modal(trigger_id, standup_name):
+def post_standup_dialog_modal(trigger_id, user_id, standup_name, redis_client):
     elements = []
+    report = []
+    if redis_client:
+        report = json.loads(get_standup_report_for_user(standup_name, user_id, redis_client))
+
     for question in get_standup_questions(standup_name):
         truncated_question = question
         if len(truncated_question) > DIALOG_LABEL_MAX_LENGTH:
             truncated_question = question[0:DIALOG_LABEL_MAX_LENGTH-3] + '...'
+
+        default_value = None
+        for submitted_question, submitted_answer in report:
+            if submitted_question == question:
+                default_value = submitted_answer
         elements.append({
             "type": "textarea",
             "label": truncated_question,
             "hint": question,
             "name": question,
-            "optional": True
+            "optional": True,
+            "value": default_value
         })
     dialog = {
         "trigger_id": trigger_id,
