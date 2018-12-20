d40682c44d9eb244121ba99ec2efd86c07d761f7
Natasha Dalal <natashadalal@gmail.com>
2018-12-20 00:04:07 -0500

    Refactor Jobs Code.

--- standup_bot/jobs/post_standup.py
+++ standup_bot/jobs/post_standup.py
@@ -1,28 +1,18 @@
 import redis
 
+from standup_bot.jobs.base_job import BaseJob
 from standup_bot.helpers import post_standup_report
-from standup_bot.config import read_config
-from standup_bot.jobs.job_helper import (
-    parse_args,
-    skip_job
-)
 
 
-def do_main():
-    args = parse_args()
+class PostStandupJob(BaseJob):
+    def do_job(self, config, standup_name):
+        redis_client = None
+        if config.get('REDIS_URL'):
+            redis_client = redis.from_url(config.get('REDIS_URL'))
 
-    if skip_job(args):
-        print("Skipping Job")
-        return
-
-    config = read_config()
-
-    redis_client = None
-    if config.get('REDIS_URL'):
-        redis_client = redis.from_url(config.get('REDIS_URL'))
-
-    post_standup_report(args.standup[0], redis_client)
+        post_standup_report(standup_name, redis_client)
 
 
 if __name__ == "__main__":
-    do_main()
+    post_standup_job = PostStandupJob()
+    post_standup_job.run_job()
